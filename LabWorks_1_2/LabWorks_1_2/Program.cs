390afc6c3f549506043c215685051d7d941b448a
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-03-21 16:55:26 +0200

    Setup database connection

--- LabWorks_1_2/LabWorks_1_2/Program.cs
+++ LabWorks_1_2/LabWorks_1_2/Program.cs
@@ -1,6 +1,18 @@
+using Core;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<IMongoClient>(s =>
+    new MongoClient(builder.Configuration.GetValue<string>("StudentStoreDatabaseSettings:ConnectionString")));
+
+builder.Services.AddSingleton<ILibraryDatabaseSettings>(sp => sp.GetRequiredService<IOptions<LibraryDatabaseSettings>>().Value);
+
+builder.Services.Configure<LibraryDatabaseSettings>(builder.Configuration.GetSection(nameof(LibraryDatabaseSettings)));
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
