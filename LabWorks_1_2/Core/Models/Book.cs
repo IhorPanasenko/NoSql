139188f5b885437137e9555fe21afff0860661eb
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-03-21 16:57:21 +0200

    Create Book Model

---
+++ LabWorks_1_2/Core/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class Book
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public DateTime WhenWasWrote { get; set; }
+
+        public string Author { get; set; }
+
+    }
+}
