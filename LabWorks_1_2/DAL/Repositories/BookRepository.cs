2e1928ce96ef740b9d81cebacb6cc072a61f1653
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-03-21 16:57:55 +0200

    Create Class for book repository

---
+++ LabWorks_1_2/DAL/Repositories/BookRepository.cs
@@ -0,0 +1,38 @@
+﻿using Core.Models;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        public Book Create(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(string bookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Book> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book GetById(string bookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(string bookId, Book book)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
