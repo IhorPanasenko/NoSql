efc9f2e20a62b20e48101e9c7a116abe342753d3
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-03-21 17:21:34 +0200

    Add Bson to properties

--- LabWorks_1_2/Core/Models/Book.cs
+++ LabWorks_1_2/Core/Models/Book.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +10,21 @@ namespace Core.Models
 {
     public class Book
     {
-        public string Id { get; set; }
-        public string Title { get; set; }
-        public DateTime WhenWasWrote { get; set; }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = String.Empty;
 
-        public string Author { get; set; }
+        [BsonElement("title")]
+        public string Title { get; set; } = String.Empty;
+
+        [BsonElement("when_was_created")]
+        public DateTime WhenWasCreated { get; set; }
+
+        [BsonElement("author")]
+        public string Author { get; set; } = String.Empty;
+
+        [BsonElement("number_of_pages")]
+        public int NumberOfPages { get; set; }
 
     }
 }
